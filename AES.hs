d24fb4395e90e4b858d10db96c9a5439b10cf289
Aaron Dufour <thedufer@gmail.com>
2015-08-22 23:11:09 -0400

    add AES encrypt functions
    ecb and cbc

--- AES.hs
+++ AES.hs
@@ -1,4 +1,4 @@
-module AES (aesInit, ecbDecrypt, cbcDecrypt, ecbEncrypt) where
+module AES (aesInit, ecbDecrypt, cbcDecrypt, ecbEncrypt, cbcEncrypt) where
 
 import Crypto.Cipher.AES
 import Data.Word
@@ -13,7 +13,7 @@ ecbDecrypt :: AES -> [Word8] -> [Word8]
 ecbDecrypt aes = B.unpack . decryptECB aes . B.pack
 
 ecbEncrypt :: AES -> [Word8] -> [Word8]
-ecbEncrypt aes = B.unpack . decryptECB aes . B.pack
+ecbEncrypt aes = B.unpack . encryptECB aes . B.pack
 
 cbcBlockDecrypt :: AES -> [Word8] -> [Word8] -> [Word8]
 cbcBlockDecrypt aes iv cipher = longXor (ecbDecrypt aes cipher) iv
@@ -22,3 +22,9 @@ cbcDecrypt :: AES -> [Word8] -> [Word8] -> [Word8]
 cbcDecrypt aes iv cipher =
   let blockedCipher = (groupsOfSize 16 cipher)
   in concat $ map (uncurry $ cbcBlockDecrypt aes) $ zip (iv:blockedCipher) blockedCipher
+
+cbcBlockEncrypt :: AES -> [Word8] -> [Word8] -> [Word8]
+cbcBlockEncrypt aes iv plain = ecbEncrypt aes $ longXor plain iv
+
+cbcEncrypt :: AES -> [Word8] -> [Word8] -> [Word8]
+cbcEncrypt aes iv plain = concat $ drop 1 $ reverse $ foldl (\ciphers -> \plain -> (cbcBlockEncrypt aes (head ciphers) plain):ciphers) [iv] (groupsOfSize 16 plain)
