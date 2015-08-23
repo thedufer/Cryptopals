47dc720ef86762dda8dc4410effe24f1ae4bcd48
Aaron Dufour <thedufer@gmail.com>
2015-08-22 22:56:40 -0400

    cleanup One/Six a bit

--- One/Six/Main.hs
+++ One/Six/Main.hs
@@ -4,8 +4,5 @@ import Xor (bestMultiByteXor, bestNByteXor)
 import Distance (normalizedDistsBetweenBlocks)
 import Util (dropNewlines)
 
-dropNewlines :: String -> String
-dropNewlines = filter (/= '\n')
-
 main :: IO ()
 main = (readFile "One/Six/input.txt") >>= putStrLn . bytesToString . bestMultiByteXor 2 40 . base64StringToBytes . dropNewlines
