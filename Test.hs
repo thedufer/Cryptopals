52a4a93ab6bb6b7a8efc2a3af29dac8f729dae68
Aaron Dufour <thedufer@gmail.com>
2015-08-22 22:43:11 -0400

    remove files that shouldn't still be around

--- Test.hs
+++
@@ -1,7 +0,0 @@
-import StringByteConversion (base64StringToBytes, bytesToBase64String)
-
-dropNewlines :: String -> String
-dropNewlines = filter (/= '\n')
-
-main :: IO ()
-main = (readFile "One/Six/input.txt") >>= putStrLn . bytesToBase64String . base64StringToBytes . dropNewlines
